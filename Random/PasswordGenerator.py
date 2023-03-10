a399f2ea71dd338e10e2d0e9b747e1d5cd53a88d
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2023-03-09 23:21:39 -0300

    Estrutura de geração de senhas transformadas em método

--- Random/PasswordGenerator.py
+++ Random/PasswordGenerator.py
@@ -1,10 +1,26 @@
 import random
 
-characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '@', '!', '#', '$', '%', '&', '*', '|']
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+              'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+              'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+              'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 
+              'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+              'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+              'W', 'X', 'Y', 'Z', '1', '2', '3', '4', 
+              '5', '6', '7', '8', '9', '0', '@', '!', 
+              '#', '$', '%', '&']
 
-password = ""
+def gerarSenha():
+    password = ""
+    num = int(input("Digite o tamanho de caracteres de sua senha (min 8, max 16): "))
+    if num < 8 or num > 16:
+        print("Número fora dos limites! \nTente Novamente! ")
+    else:
+        print("Digito não identificado!")
+    if num >= 8 or num <= 16:
+        for x in range(num):
+            password = password + random.choices(characters)[0]
+        print("Sua senha gerada aleatoriamente: ", password)
+    return password
 
-for x in range(16):
-    password = password + random.choices(characters)[0]
-
-print("Sua senha gerada aleatoriamente: ", password)
+gerarSenha()
